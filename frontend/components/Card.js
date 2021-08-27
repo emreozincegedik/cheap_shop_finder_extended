10165e91e913ce27e77779a3c7be8ece036813a1
Emre Özincegedik <emreozincegedik@gmail.com>
2021-08-27 23:05:13 +0300

    card styling

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import styles from '../styles/card.module.css'
+import { useRouter } from 'next/router'
 
-export default function Card({img,title,price,link,website}) {
+export default function Card({ img, title, price, link, website }) {
+  const router=useRouter()
   return (
-    <div  className="container d-flex flex-row card" style={{"marginTop":"1rem"}}>
-      <div className="col-lg-3">
-        <img alt={title || "item_title"} src={img || "https://childrenheartcare.com/wp-content/uploads/2019/08/ASD-TYPES.jpg"} className={styles.item_photo}/>
+    <div  className={styles.container + " container d-flex flex-row card"}>
+      <div className="col-lg-2">
+        <img alt={title || "item_title"}
+          src={img || "https://via.placeholder.com/250x250"}
+          className={styles.item_photo} />
       </div>
-      <div className="col-lg-8">
-        <h3>title</h3>
-        <p>price</p>
-      </div>
-      <div className="col-lg-1">
-        <button className="btn btn-block btn-primary">asd</button>
+      <div className={styles.nonImg+" col-lg-10 d-flex flex-column justify-content-around"}>
+        <h3 className={styles.title}>{title || "title"}</h3>
+        <div className="container d-flex flex-row align-self-center">
+
+          <h4 className={styles.price+" col-lg-6"}>{price || "price"}</h4>
+          <h4 className={" col-lg-6"}>TL</h4>
+        </div>
+        <button className={"btn btn-block btn-outline-primary " + styles.button}
+          onClick={() => {
+            console.log((link || "asd"))
+            router.push((link || "/"))
+          }}>{website || "website"}</button>
       </div>
     </div>
   )
