e2588bc21b15cd6cecbb165a2742dffe4bf9971a
Emre Özincegedik <emreozincegedik@gmail.com>
2021-08-27 14:42:09 +0300

    filter,pagination,cardlist to page

--- frontend/pages/arama/[query]/[page].js
+++ frontend/pages/arama/[query]/[page].js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 
+import CardAndFilterBox from '../../../components/CardAndFilterBox'
+import Pagination from '../../../components/Pagination'
+
 export default function Page() {
   const router = useRouter()
   const { page } = router.query
   return (
-    <div>
+    <>
       Arama sayfa: {page}
-    </div>
+      <CardAndFilterBox />
+      <Pagination/>
+    </>
   )
 }
--- frontend/pages/arama/[query]/index.js
+++ frontend/pages/arama/[query]/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 
+import CardAndFilterBox from '../../../components/CardAndFilterBox'
+import Pagination from '../../../components/Pagination'
+
 export default function Query() {
   const router = useRouter()
   const { query } = router.query
   return (
-    <div>
+    <>
       Arama: {query}
-    </div>
+      <CardAndFilterBox />
+      <Pagination/>
+    </>
   )
 }
