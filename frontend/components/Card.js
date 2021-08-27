e016e80979f52ea68cd956d93293054748fdaf10
Emre Özincegedik <emreozincegedik@gmail.com>
2021-08-27 14:42:38 +0300

    card style

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from '../styles/card.module.css'
 
 export default function Card({img,title,price,link,website}) {
   return (
     <div  className="container d-flex flex-row card" style={{"marginTop":"1rem"}}>
       <div className="col-lg-3">
-        <img alt={title || "item_title"} src={ img||"https://childrenheartcare.com/wp-content/uploads/2019/08/ASD-TYPES.jpg" } className="item_photo"/>
+        <img alt={title || "item_title"} src={img || "https://childrenheartcare.com/wp-content/uploads/2019/08/ASD-TYPES.jpg"} className={styles.item_photo}/>
       </div>
       <div className="col-lg-8">
         <h3>title</h3>
